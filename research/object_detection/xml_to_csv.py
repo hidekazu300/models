6ba05fe8743dd01d31339b506d537041f2b6069c
hidekazu300 <34515037+hidekazu300@users.noreply.github.com>
2019-12-09 22:07:25 +0900

    Update xml_to_csv.py

--- research/object_detection/xml_to_csv.py
+++ research/object_detection/xml_to_csv.py
@@ -14,11 +14,11 @@ def xml_to_csv(path):
                      int(root.find('size')[0].text),
                      int(root.find('size')[1].text),
                      member[0].text,
-                     int(member[4][0].text),
-                     int(member[4][1].text),
-                     int(member[4][2].text),
-                     int(member[4][3].text)
-                     )
+                     int(member[1][0].text),
+                     int(member[1][1].text),
+                     int(member[1][2].text),
+                     int(member[1][3].text)
+                     )#形を合わせるため、４→1
             xml_list.append(value)
     column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
     xml_df = pd.DataFrame(xml_list, columns=column_name)
